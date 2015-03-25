e54b6efeb8a3866e6658dd6065af4a7b1838da4f
C.M Kendrick <cindy@cleverness.org>
2015-03-25 15:08:20 -0400

    fixed todochecklist progress not 100% when completed

--- includes/cleverness-to-do-list-frontend.class.php
+++ includes/cleverness-to-do-list-frontend.class.php
@@ -429,7 +429,7 @@ class CTDL_Frontend_Checklist extends ClevernessToDoList {
 				$this->list .= '<div class="todoitem">';
 				$this->show_todo_text( get_the_content(), 'list' );
 				if ( $atts['priority'] == 1 ) $this->show_priority( $the_priority );
-				if ( $atts['progress'] == 1 ) $this->show_progress( $progress_meta );
+				if ( $atts['progress'] == 1 ) $this->show_progress( $progress_meta, 'list', $completed );
 				if ( $atts['assigned'] == 1 ) $this->show_assigned( $assign_meta );
 				if ( $atts['addedby'] == 1 ) $this->show_addedby( get_the_author() );
 				if ( $atts['deadline'] == 1 ) $this->show_deadline( $deadline_meta );
